2bfcc6cbad5032ba13cd6175b148769e360ec83e
Jesvin Jose <frank.einstien@gmail.com>
2022-05-01 21:12:15 +0530

    Custom user class with balance. Endpoint and tests for checking if credentials are valid.

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -1,8 +1,16 @@
 from django.test import Client, TestCase
 
-# Create your tests here.
-class WalletCreationTestCase(TestCase):
-    def test_index(self):
+from app.models import User
+
+
+class LoginTestCase(TestCase):
+    def test_user_can_login_with_correct_credentials_(self):
         client = Client()
-        response = client.get('/home')
-        self.assertEqual(response.status_code, 200)
+        user = User.objects.create_user(username='foo', password='bar', balance=0)
+        response = client.post('/login', {'username': 'foo', 'password': 'bar'})
+        self.assertTrue(response.json()['has_logged_in'])
+
+    def test_login_with_incorrect_credentials_fail(self):
+        client = Client()
+        response = client.post('/login', {'username': 'foo', 'password': 'bar'})
+        self.assertFalse(response.json()['has_logged_in'])
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.http import HttpResponse, JsonResponse
+from app.models import User
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    print('user', user, username, password)
+    return JsonResponse({'has_logged_in': bool(user)})
