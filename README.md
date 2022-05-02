f44569a9bed418e21aae5c3a7818e8f077c9903a
Jesvin Jose <frank.einstien@gmail.com>
2022-05-02 22:24:37 +0530

    Started frontend and skeleton of login.

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # FS challenge
 
-Run these steps to run the code. 
+Run these steps to run the backend. 
 
 ```shell
 $ cd backend
 $ pip install -r requirements.txt
 $ python3 manage.py migrate
+# Test everything is working as intended
 $ python3 manage.py test app
+# Run local server
 $ python3 manage.py runserver
 ```
 
+```shell
+ $ cd frontend
+ $ npm run serve
+
+```
---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h1 class="title">Lending Tool</h1>
+    <div v-if="!username">
+      login screen
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import Buefy from "buefy";
+import Vue from "vue";
+import "buefy/dist/buefy.css";
+
+import HelloWorld from "./components/HelloWorld.vue";
+
+Vue.use(Buefy);
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      username: null,
+      transactions: null,
+      loginInput: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {},
+};
+</script>
